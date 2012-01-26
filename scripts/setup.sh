25e878ad75d4a7816d757900ef3ffdc39d88a035
Lloyd Henning <thunderbird>
2012-01-26 20:18:22 +0000

    setup now sets up for local user, e.g., for cs usage

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,29 +2,29 @@
 set -o errexit
 set -o nounset
 
-sudo apt-get --assume-yes install \
-    git \
-    python-dev \
-    python-setuptools
-
 # libfoxxy
-sudo easy_install https://github.com/dj-foxxy/libfoxxy/zipball/master
+easy_install --user \
+    https://github.com/dj-foxxy/libfoxxy/zipball/master
 
 # simplejson
-sudo easy_install https://github.com/simplejson/simplejson/zipball/master
+easy_install --user \
+    https://github.com/simplejson/simplejson/zipball/master
 
 # httplib2
-sudo easy_install http://httplib2.googlecode.com/files/httplib2-0.7.2.tar.gz
+easy_install --user \
+    http://httplib2.googlecode.com/files/httplib2-0.7.2.tar.gz
 
 # oauth
-sudo easy_install https://github.com/simplegeo/python-oauth2/tarball/master
+easy_install --user \
+    https://github.com/simplegeo/python-oauth2/tarball/master
 
 # Python twitter
-sudo easy_install http://python-twitter.googlecode.com/files/python-twitter-0.8.2.tar.gz
+easy_install --user \
+    http://python-twitter.googlecode.com/files/python-twitter-0.8.2.tar.gz
 
 # pyutmp
 cd /tmp
 git clone git://github.com/bmc/pyutmp.git
 cd pyutmp
-sudo python setup.py install
+python setup.py install
 
