00dae619df1b2573760fba0d37784d156a6cfc69
Lloyd Henning <lloyd.w.henning@gmail.com>
2012-06-21 18:06:48 +0100

    Added more downloads for tk

--- scripts/setup-arch.sh
+++ scripts/setup-arch.sh
@@ -3,7 +3,7 @@
 set -o errexit
 set -o nounset
 
-sudo pacman -Sy --noconfirm python2 python-imaging python2-pip
+sudo pacman -Sy --noconfirm python2 python-imaging python2-pip tk
 
 sudo easy_install-2.7 httplib2 oauth2 python-twitter simplejson
 
