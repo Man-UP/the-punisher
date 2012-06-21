b42e9eab78f34b56ab0814e8579e36535480ed29
Lloyd Henning <lloyd.w.henning@gmail.com>
2012-06-21 17:51:26 +0100

    Added noconfirm to arch install and python2-pip and python2

--- scripts/setup-arch.sh
+++ scripts/setup-arch.sh
@@ -3,7 +3,7 @@
 set -o errexit
 set -o nounset
 
-sudo pacman -Sy python-imaging
+sudo pacman -Sy --noconfirm python2 python-imaging python2-pip
 
 sudo easy_install-2.7 httplib2 oauth2 python-twitter simplejson
 
